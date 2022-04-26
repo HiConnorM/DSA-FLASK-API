c66665c2c0adb0b3ec3834d4801f89aabdb6642d
Connor Morgan <theconnorm@gmail.com>
2022-04-26 11:52:21 -0500

    updates to finished linked list in API Ability to add to head and end of LL get users from ascending to descending order add one user and delete one user

--- linked_list.py
+++ linked_list.py
@@ -1,14 +1,17 @@
+# Node class to create the nodes which store the data and pointer
 class Node:
     def __init__(self, data=None, next_node=None):
         self.data = data
         self.next_node = next_node
 
 
+# Wrapper for Linked List class to create the linked list head and tail.
 class LinkedList:
     def __init__(self):
         self.head = None
         self.last_node = None
 
+# for creating the array(list for python) that will store the user data in a list from a dictionary
     def to_list(self):
         l = []
         if self.head is None:
@@ -34,7 +37,7 @@ class LinkedList:
         ll_string += " None"
         print(ll_string)
 
-#Linked List Insert:
+# Linked List Insert at beginning:
 
     def insert_beginning(self, data):
         if self.head is None:
@@ -44,7 +47,7 @@ class LinkedList:
         new_node = Node(data, self.head)
         self.head = new_node
 
-
+# Linked List Insert at end:
     def insert_at_end(self, data):
         if self.head is None:
             self.insert_beginning(data)
@@ -53,6 +56,14 @@ class LinkedList:
         self.last_node.next_node = Node(data, None)
         self.last_node = self.last_node.next_node
 
+# To get one user id
+    def get_user_by_id(self, user_id):
+        node = self.head
+        while node:
+            if node.data["id"] is int(user_id):
+                return node.data
+            node = node.next_node
+        return None
 
 
 
