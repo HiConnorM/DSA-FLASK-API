55b8ebda5fc6dc0583ba52364f5f56432e8c56ee
Connor Morgan <theconnorm@gmail.com>
2022-04-28 12:51:49 -0500

    Hash Table formed

--- hash_table.py
+++ hash_table.py
@@ -20,3 +20,52 @@ class HashTable:
             hash_value = (hash_value * ord(i)) % self.table_size
         return hash_value
 
+    def add_key_value(self, key, value):
+        hashed_key = self.custom_hash(key)
+        if self.hash_table[hashed_key] is None:
+            self.hash_table[hashed_key] = Node(Data(key, value), None)
+        else:
+            node = self.hash_table[hashed_key]
+            while node.next_node:
+                node = node.next_node
+
+            node.next_node = Node(Data(key, value), None)
+
+    def get_value(self, key):
+        hashed_key = self.custom_hash(key)
+        if self.hash_table[hashed_key] is not None:
+            node = self.hash_table[hashed_key]
+            if node.next_node is None:
+                return node.data.value
+            while node.next_node:
+                if key == node.data.value:
+                    return node.data.value
+                node = node.next_node
+
+            if key == node.data.key:
+                return node.data.value
+        return None
+
+    def print_table(self):
+        print("{")
+        for i, val in enumerate(self.hash_table):
+            if val is not None:
+                llist_string = ""
+                node = val
+                if node.next_node:
+                    while node.next_node:
+                        llist_string += (
+                            str(node.data.key) + " : " + str(node.data.value) + " --> "
+                        )
+                        node = node.next_node
+                    llist_string += (
+                            str(node.data.key) + " : " + str(node.data.value) + " --> None"
+                    )
+                    print(f"   [{i}] {llist_string}")
+                else:
+                    print(f"   [{i}] {val.data.key} : {val.data.key}")
+            else:
+                print(f"    [{i}] {val}")
+        print("}")
+
+
